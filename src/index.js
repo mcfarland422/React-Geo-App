8e1915ab1abbd14ab016417042ec6efdaacd7cec
mcfarland422 <mcfarlandweb@gmail.com>
2020-10-17 10:02:18 -0600

    Season config

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // Create a reacte component
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = { lat: null, errorMessage: '' };
-
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
     
+    
+        this.state = { lat: null, errorMessage: '' };
     }
+
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+        
+    
+    
 
     
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat} <br />
-                Error: {this.state.errorMessage}
-            </div>
-        );
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div>Loading!</div>;
     }
         
         
