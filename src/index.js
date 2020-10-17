c728861434208baac70c63c1ce07cadc9f241710
mcfarland422 <mcfarlandweb@gmail.com>
2020-10-17 11:26:11 -0600

    Final Functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // Create a reacte component
 
@@ -35,7 +36,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>;
+        return <Spinner message="Please Accept Location Request" />;
     }
         
         
